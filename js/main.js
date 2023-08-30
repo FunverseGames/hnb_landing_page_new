357fbba5c8fbe5c52946909419a359b6dbab5021
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2023-08-30 23:19:35 +0300

    project update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+window.onload = function () {
+    try {
+        var url_string = (window.location.href).toLowerCase();
+        var url = new URL(url_string);
+        var tab = url.searchParams.get("tab");
+        if (tab === "story" || tab === "patch") {
+            document.getElementById(tab + "-button").click();
+        } else {
+            document.getElementById("defaultOpen").click();
+        }
+    } catch (err) {
+        console.log("Issues with Parsing URL Parameter's - " + err);
+    }
+}
+
 
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
@@ -48,3 +63,21 @@ wepBtn3.addEventListener("click", () => {
     wepText2.classList.remove("wep-active");
     wepText3.classList.toggle("wep-active");
 });
+
+
+
+// haberler tab 
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("blog-list-button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" list-active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " list-active";
+}
+
