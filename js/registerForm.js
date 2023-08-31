b25d0664d8316cfbb73250291018d9c42a7159b1
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2023-08-31 22:55:02 +0300

    catching utm params added.

--- js/registerForm.js
+++ js/registerForm.js
@@ -13,6 +13,13 @@ registerForm.addEventListener("submit", async (e) => {
     let loader = document.getElementById("loader")
     let submitButton = document.getElementById("submit-button")
 
+    let utm_id = document.getElementById("utm_id").value;
+    let utm_source = document.getElementById("utm_source").value;
+    let utm_medium = document.getElementById("utm_medium").value;
+    let utm_campaign = document.getElementById("utm_campaign").value;
+    let utm_term = document.getElementById("utm_term").value;
+    let utm_content = document.getElementById("utm_content").value;
+
     //resetting form state
     formFeedback.style.display = "none";
     formFeedbackMessage.innerText = "";
@@ -41,11 +48,20 @@ registerForm.addEventListener("submit", async (e) => {
         formFeedbackMessage.innerText = "Lütfen gizlilik politikamızı kabul ediniz!"
         return;
     }
+    let referrer = JSON.stringify({
+        utm_id: utm_id ? utm_id : "NaN",
+        utm_source: utm_source ? utm_source : "NaN",
+        utm_medium: utm_medium ? utm_medium : "NaN",
+        utm_campaign: utm_campaign ? utm_campaign : "NaN",
+        utm_term: utm_term ? utm_term : "NaN",
+        utm_content: utm_content ? utm_content : "NaN",
+    });
 
     const payload = {
         email,
         policy: acceptPolicy,
-        receive: acceptReceive
+        receive: acceptReceive,
+        referrer
     }
 
     const url = "https://old-hitandboom.vercel.app/api/register"
@@ -68,8 +84,8 @@ registerForm.addEventListener("submit", async (e) => {
         submitButton.style.display = "block"
         formFeedback.style.display = "block";
         formFeedbackMessage.innerText = result.message;
-        if(response.status===201){
-            formFeedback.style.backgroundColor="#16a34a"
+        if (response.status === 201) {
+            formFeedback.style.backgroundColor = "#16a34a"
         }
         return;
     }
